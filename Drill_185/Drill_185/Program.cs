298f0d548bd03a24c18f7b5746be96756ba11ffb
Carlos Ramirez <carlosramir991@gmail.com>
2020-11-18 20:38:22 -0700

    Drill_185

---
+++ Drill_185/Drill_185/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Drill_185
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+
+            Console.WriteLine("Please Enter the package weight.");
+
+            string Input1 = Console.ReadLine();
+            int PackageWeight = Convert.ToInt32(Input1);
+
+
+            if (PackageWeight > 50) {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                return;
+            }
+            
+
+
+            Console.WriteLine("Please Enter the package width.");
+            string Input2 = Console.ReadLine();
+            int PackageWidth = Convert.ToInt32(Input2);
+
+
+            Console.WriteLine("Please Enter the package length.");
+            string Input3 = Console.ReadLine();
+            int PackageLength = Convert.ToInt32(Input3);
+
+
+            if (PackageLength >= 50 || PackageWidth >= 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                return;
+            } 
+
+            double price = PackageLength * PackageWeight * PackageWidth;
+            int finalPrice = (int)price / 100;
+            Console.WriteLine("Your Your estimated total for shipping this package is: $" + finalPrice +".00" +
+               " Thank you!");
+
+
+
+        }
+    }
+}
