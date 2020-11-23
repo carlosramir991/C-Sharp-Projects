08aed82d68fa4a8f0a5b36e691085be91ff3860b
Carlos Ramirez <carlosramir991@gmail.com>
2020-11-22 20:25:19 -0700

    Drill_214

---
+++ Drill_223/Drill_223/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Drill_223
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter a number!");
+            int numb1;
+            numb1 = Convert.ToInt32(Console.ReadLine());
+
+            methods1 met = new methods1();
+            Console.WriteLine(numb1 + " divided by 8 equals: "+met.division(numb1));
+            Console.WriteLine(numb1 + " plus 300 is: " + met.addition(numb1));
+            Console.WriteLine(numb1 + " multiplied by 5 is: "+met.multiplication(numb1));
+        }
+
+         
+
+    
+    } 
+
+
+
+
+    
+
+    
+}
---
+++ Drill_223/Drill_223/methods1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Drill_223
+{
+    public class methods1
+    {
+        public object division(int numb1)
+        {
+
+            int answer1 = numb1 / 8;
+
+            return answer1;
+
+        }
+
+        public object addition(int numb1)
+        {
+            int answer2 = numb1 + 300;
+
+            return answer2;
+        }
+
+        public object multiplication(int numb1)
+        {
+            int answer3 = numb1 * 5;
+
+            return answer3;
+        }
+    }
+}
