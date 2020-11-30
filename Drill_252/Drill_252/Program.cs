1a55b5dcfa01876b91f529e82d03703ff30b7a5a
Carlos Ramirez <carlosramir991@gmail.com>
2020-11-30 11:38:42 -0700

    Drill_252

---
+++ Drill_252/Drill_252/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Drill_247
+{
+    class Program : employee
+    {
+        static void Main(string[] args)
+        {
+            employee met = new employee();
+            met.employee1 = "Carlos";
+            met.employee2 = "Carlos2";
+            met.id1 = 123;
+            met.id2 = 124;
+
+            Console.WriteLine(met.id1 == met.id2);
+        }
+    }
+}
+
---
+++ Drill_252/Drill_252/employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Drill_247
+{
+    public class employee 
+    {
+        public string employee1 { get; set; }
+        public string employee2 { get; set; }
+        public int id1 { get; set; }
+        public int id2 { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
