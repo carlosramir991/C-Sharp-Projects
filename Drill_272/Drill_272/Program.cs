16dfa0e450f571609e25f5bec510d92c3e1f1f22
Carlos Ramirez <carlosramir991@gmail.com>
2020-11-30 20:43:31 -0700

    Drill_272

---
+++ Drill_272/Drill_272/Program.cs
@@ -0,0 +1,65 @@
+﻿//1.In the Main() method, create a list of at least 10 employees.
+//Each employee should have a first and last name, as well as an Id.
+//At least two employees should have the first name "Joe".
+
+//2. Using a foreach loop, create a new list of all employees with the first name "Joe".
+
+//3. Perform the same action again, but this time with a lambda expression.
+
+//4. Using a lambda expression, make a list of all employees with an Id number greater than 5.
+
+
+
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Drill_272
+{
+    class Program : employees<Type>
+    {
+        static void Main(string[] args)
+        {
+            employees<string> emp = new employees<string>();
+            emp.firstName= new List<string>() { "Carlos", "Eric", "Joe", "Joe" };
+            employees<string> emp2 = new employees<string>();
+            emp2.lastName = new List<string>() { "Ramirez", "Jones", "Jonas", "Shmo" };
+            employees<int> emp3 = new employees<int>();
+            emp3.empId = new List<int> { 12, 1, 3, 8 };
+            employees<string> emp4 = new employees<string>();
+            emp4.joeEmpls = new List<string>();
+            employees<string> emp5 = new employees<string>();
+            emp5.joeEmpls = new List<string>();
+            emp5.joeEmpls = emp.firstName.Where(x => x == "Joe").ToList();
+
+            foreach (string name in emp5.joeEmpls)
+            {
+                Console.WriteLine(name);
+            }
+
+
+            foreach (string name in emp.firstName)
+            {
+                if (name == "Joe")
+                {
+                    emp4.joeEmpls.Add(name);
+                   
+                }
+                
+            }
+
+
+            
+
+
+
+
+            
+            
+
+
+
+        }
+    }
+}
---
+++ Drill_272/Drill_272/employees.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Drill_272
+{
+    public class employees<Type>
+    {
+        public List<Type> firstName { get; set; }
+        public List<Type> lastName { get; set; }
+        public List<Type> empId { get; set; }
+        public List<Type> joeEmpls { get; set; }
+
+        
+    }
+}
