c70ed0ad629d5810ca21e58eb88bf8d6b8cdb78c
Carlos Ramirez <carlosramir991@gmail.com>
2020-12-06 14:19:56 -0700

    Drill_310

---
+++ Drill_310/Drill_310/Program.cs
@@ -0,0 +1,35 @@
+﻿//1.Create a const variable.
+
+//2. Create a variable using the keyword "var.”
+
+//3.Chain two constructors together.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Drill_310
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            string fname = "Carlos";
+            Console.WriteLine("What is your last name?");
+            string lname = Console.ReadLine();
+
+            // One can also simply type
+            // id starter = new id(fname, lname)
+            // this will include the last name that one types above
+            id starter = new id(fname);
+            
+         
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Drill_310/Drill_310/id.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Drill_310
+{
+    public class id
+    {
+
+            const string fname = "Carlos";
+            string lname { get; set; }
+
+
+            public id(string fname, string lname)
+            {
+
+                Console.WriteLine("How old are you?");
+                var age = Convert.ToInt32(Console.ReadLine());
+
+
+
+                Console.WriteLine("Hello " +fname + " " + lname + " You are " +age+" years old.");
+
+            }
+            public id(string fname) : this(fname, "Rodriguez")
+            {
+
+            }
+       
+    }
+}
