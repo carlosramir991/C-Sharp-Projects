1e9e84f423c0f178bca6c1a890b434bf1f0a6596
Carlos Ramirez <carlosramir991@gmail.com>
2020-12-06 17:37:09 -0700

    Drill_314

---
+++ Drill_314/Drill_314/AgeError.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Drill_314
+{
+    public class AgeError:Exception
+    {
+        public AgeError():base()
+        {
+            Console.WriteLine("You must enter a number greater than 0! "+"\n"+"now..");
+        }
+
+    }
+}
---
+++ Drill_314/Drill_314/Program.cs
@@ -0,0 +1,59 @@
+﻿//1.Ask the user for their age.
+
+//2. Display the year the user was born.
+
+//3. Exceptions must be handled using “try/catch.”
+
+//4.Display appropriate error messages if the user enters zero or negative numbers.
+
+//5.Display a general message if an exception was caused by anything else.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Drill_314
+{
+    class Program
+    {
+        private static int age;
+
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+
+                Console.WriteLine("Hello! Please enter your age.");
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age < 1)
+                {
+                     AgeError();
+                    
+                }
+                var today = DateTime.Today;
+                var born = today.Year - age;
+                Console.WriteLine("You were born in " + born);
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a valid age.");
+            }
+            
+
+
+
+
+            
+
+
+
+        }
+
+        private static void AgeError()
+        {
+            throw new AgeError();
+        }
+    }
+}
